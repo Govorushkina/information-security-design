e4692ab228a202b9c4d2967af0f23333d3f585c0
Govorushkina <112868446+Govorushkina@users.noreply.github.com>
2024-12-17 23:52:10 +0300

    Task 1.7 refactor
    Добавлен атрибут "Номер водительского удостоверения", для  сравнения по нему.

--- BaseDriver.py
+++ BaseDriver.py
@@ -4,20 +4,20 @@ import re
 
 
 class BaseDriver:
-    def __init__(self, last_name, first_name,  sur_name, phone_number, birthday,  driver_id=None):
-        self.driver_id = driver_id
+    def __init__(self, last_name, first_name,  sur_name, phone_number,driver_license,  driver_id=None):
+        self.set_driver_id(driver_id)
         self.set_last_name(last_name)
         self.set_first_name(first_name)
         self.set_sur_name(sur_name)
         self.set_phone_number(phone_number)
-        self.set_birthday(birthday)
+        self.set_driver_license(driver_license)
 
     @staticmethod
     def validate_string(value: str):
         if not isinstance(value, str) or len(value.strip()) == 0:
             return False
         return True
-#
+
     @staticmethod
     def validate_birthday(birthday) -> bool:
         return isinstance(birthday, datetime)
@@ -25,6 +25,13 @@ class BaseDriver:
     @staticmethod
     def validate_phone_number(phone_number):
         return isinstance(phone_number, str) and (re.fullmatch(r"^89\d{9}$", phone_number))
+
+    @staticmethod
+    def validate_license(document):
+        return isinstance(document, int) and re.fullmatch(rf"^\d{{{6}}}$", str(document))
+
+    def get_driver_license(self):
+        return self.__driver_license
 
     def get_last_name(self):
         return self.__last_name
@@ -38,9 +45,11 @@ class BaseDriver:
     def get_phone_number(self):
         return self.__phone_number
 
-    def get_birthday(self):
-        return self.__birthday
+    def get_driver_id(self):
+        return self.__driver_id
 
+    def set_driver_id(self, driver_id: int):
+        self.__driver_id = driver_id
 
     def set_last_name(self, last_name):
         if not self.validate_string(last_name):
@@ -62,43 +71,31 @@ class BaseDriver:
             raise ValueError("Номер телефона введен неверно(Номер телефона должен начинаться с 89 и содержать 11 цифр).")
         self.__phone_number = phone_number
 
-    def set_birthday(self, birthday):
-        if self.validate_birthday(birthday):
-            raise ValueError("Дата рождения не может быть пустой.")
-        self.__birthday = birthday
+    def set_driver_license(self, driver_license: int):
+        if not self.validate_license(driver_license):
+            raise ValueError("Неверно введены данные")
+        self.__driver_license = driver_license
 
     def __repr__(self):
-        return (f"Driver(last_name='{self.get_last_name()}', "
-                f"first_name='{self.get_first_name()}', sur_name='{self.get_sur_name()}', "
-                f"phone_number={self.get_phone_number()})")
+        return (f"Driver('{self.get_last_name()}', '{self.get_first_name()}', '{self.get_sur_name()}', '{self.get_phone_number()}', "
+                f"{self.get_driver_license()}, {self.get_driver_id()})")
 
     def __str__(self):
         return (f"Driver Name: {self.get_last_name()} {self.get_first_name()} {self.get_sur_name()}, "
-                f"phone_number: {self.get_phone_number()} ,birthday: {self.get_birthday()} ")
-
-    @property
-    def short_version(self):
-        return f"Driver({self.get_first_name()} {self.get_last_name()})"
-
-    @property
-    def full_version(self):
-        return (f"Driver(Driver:{self.get_first_name()} {self.get_last_name()} {self.get_sur_name()}, "
-                f"birthday:{self.get_birthday()}, "
-                f"phone_number:{self.get_phone_number()})")
+                f"phone_number: {self.get_phone_number()} ")
 
     def __eq__(self, other):
         if isinstance(other, BaseDriver):
-            return (self.get_phone_number() == other.get_phone_number())
+            return (self.get_driver_license() == other.get_driver_license())
         return False
 
 
 class Driver(BaseDriver):
-    def __init__(self, last_name, first_name, sur_name, phone_number, birthday, experience, insurance_policy,
-                 driver_license, vehicle_passport, driver_id=None):
-        super().__init__(last_name, first_name, sur_name, phone_number, birthday, driver_id)
+    def __init__(self, last_name, first_name, sur_name, phone_number,driver_license, experience, insurance_policy,
+                 vehicle_passport, driver_id=None):
+        super().__init__(last_name, first_name, sur_name, phone_number, driver_license, driver_id)
         self.set_experience(experience)
         self.set_insurance_policy(insurance_policy)
-        self.set_driver_license(driver_license)
         self.set_vehicle_passport(vehicle_passport)
 
     @classmethod
@@ -111,7 +108,6 @@ class Driver(BaseDriver):
                 first_name=data['first_name'],
                 sur_name=data['sur_name'],
                 phone_number=data['phone_number'],
-                birthday=birthday,
                 experience=data['experience'],
                 insurance_policy=data['insurance_policy'],
                 driver_license=data['driver_license'],
@@ -125,9 +121,6 @@ class Driver(BaseDriver):
 
     def get_insurance_policy(self):
         return self.__insurance_policy
-
-    def get_driver_license(self):
-        return self.__driver_license
 
     def get_vehicle_passport(self):
         return self.__vehicle_passport
@@ -147,16 +140,10 @@ class Driver(BaseDriver):
             raise ValueError("Стаж не может быть отрицательным значением.")
         self.__experience = experience
 
-
     def set_insurance_policy(self, insurance_policy: int):
         if not self.validate_document(insurance_policy, 10):
             raise ValueError("Неверно введены данные")
         self.__insurance_policy = insurance_policy
-
-    def set_driver_license(self, driver_license: int):
-        if not self.validate_document(driver_license, 6):
-            raise ValueError("Неверно введены данные")
-        self.__driver_license = driver_license
 
     def set_vehicle_passport(self, vehicle_passport: int):
         if not self.validate_document(vehicle_passport, 15):
@@ -165,3 +152,4 @@ class Driver(BaseDriver):
     @property
     def short_description(self):
         return self.get_last_name(), self.get_phone_number()
+
